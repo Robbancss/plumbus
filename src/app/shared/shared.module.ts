d9bebfd2091adb6544a619d929b80715ed82066e
robert.panczel <robi.panczel@gmail.com>
2021-01-29 15:51:20 +0100

    Menu with links added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import {RouterModule} from '@angular/router';
+import {MatListModule} from '@angular/material/list';
 
 
 
 @NgModule({
   declarations: [LayoutComponent],
-    imports: [
-        CommonModule,
-        RouterModule
-    ]
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatListModule
+  ],
+  exports: [LayoutComponent]
 })
 export class SharedModule { }
